407b37e842a75e36a75c10f09117e2e01d100214
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-20 00:51:28 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -20,10 +20,10 @@ class Person
   end
   def ballin_candy_lover?
     if likes_candy?
-      puts "Aw sweet, this cat loves them some candy!"
+      "Aw sweet, this cat loves them some candy!"
     else
       10.times do
-        puts "not a ballin candy lover"
+        "not a ballin candy lover"
     end
   end
 end
